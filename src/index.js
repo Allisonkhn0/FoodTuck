43a8d1bdd270bba3d11a9826e22d03ed4b3b45bb
unknown <kostechkca123@gmail.com>
2025-10-20 15:25:12 +0700

    NEW: сделал бургер меню у main. MODIFIED: улучшена адаптивность на 320px

--- src/index.js
+++ src/index.js
@@ -4,3 +4,23 @@ function scrollUp() {
     behavior: 'smooth'
   });
 }
+
+let burger = document.querySelector('.burger');
+let list = document.querySelector('.list');
+
+burger.addEventListener('click', e => {
+  e.stopPropagation();
+  list.classList.toggle('active')
+  console.log('Клик!');
+})
+
+document.addEventListener('click', e => {
+
+	if (!list.contains(e.target)) {
+		list.classList.remove('active');
+    console.log('Чтот не так');
+	}
+
+  console.log('Угабуга политех');
+
+});
